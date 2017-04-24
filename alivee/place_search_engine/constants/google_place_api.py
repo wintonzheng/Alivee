169ef36ed8ac7878a41a47301eccf6a11fb27d25
cuiyang36 <wangyang19901026@gmail.com>
2017-04-24 01:20:17 -0700

    need to add package to script

--- alivee/place_search_engine/constants/google_place_api.py
+++ alivee/place_search_engine/constants/google_place_api.py
@@ -14,9 +14,9 @@ class PlaceSearchResponseStatus(object):
 	INVALID_REQUEST = 'INVALID_REQUEST'
 
 
-class PlaceSearchPlaceType(object):
+class PlaceServicePlaceType(object):
 	'''
-	Class includes all place types we can use in place search query, deprecated types are not listed.
+	Class includes all place types we can use in place service query, deprecated types are not listed.
 	Documents: https://developers.google.com/places/web-service/supported_types
 	'''
 	ACCOUNTING = 'accounting'
@@ -109,4 +109,147 @@ class PlaceSearchPlaceType(object):
 	UNIVERSITY = 'university'
 	VETERINARY_CARE = 'veterinary_care'
 	ZOO = 'zoo'
-	
+	ADMINISTRATIVE_AREA_LEVEL_1 = 'administrative_area_level_1'
+	ADMINISTRATIVE_AREA_LEVEL_2 = 'administrative_area_level_2'
+	ADMINISTRATIVE_AREA_LEVEL_3 = 'administrative_area_level_3'
+	ADMINISTRATIVE_AREA_LEVEL_4 = 'administrative_area_level_4'
+	ADMINISTRATIVE_AREA_LEVEL_5 = 'administrative_area_level_5'
+	COLLOQUIAL_AREA = 'colloquial_area'
+	COUNTRY = 'country'
+	ESTABLISHMENT = 'establishment'
+	FINANCE = 'finance'
+	FLOOR = 'floor'
+	FOOD = 'food'
+	GENERAL_CONTRACTOR = 'general_contractor'
+	GEOCODE = 'geocode'
+	HEALTH = 'health'
+	INTERSECTION = 'intersection'
+	LOCALITY = 'locality'
+	NATURAL_FEATURE = 'natural_feature'
+	NEIGHBORHOOD = 'neighborhood'
+	PLACE_OF_WORSHIP = 'place_of_worship'
+	POLITICAL = 'political'
+	POINT_OF_INTEREST = 'point_of_interest'
+	POST_BOX = 'post_box'
+	POSTAL_CODE = 'postal_code'
+	POSTAL_CODE_PREFIX = 'postal_code_prefix'
+	POSTAL_CODE_SUFFIX = 'postal_code_suffix'
+	POSTAL_TOWN = 'postal_town'
+	PREMISE = 'premise'
+	ROOM = 'room'
+	ROUTE = 'route'
+	STREET_ADDRESS = 'street_address'
+	STREET_NUMBER = 'street_number'
+	SUBLOCALITY = 'sublocality'
+	SUBLOCALITY_LEVEL_5 = 'sublocality_level_5'
+	SUBLOCALITY_LEVEL_4 = 'sublocality_level_4'
+	SUBLOCALITY_LEVEL_3 = 'sublocality_level_3'
+	SUBLOCALITY_LEVEL_2 = 'sublocality_level_2'
+	SUBLOCALITY_LEVEL_1 = 'sublocality_level_1'
+	SUBPREMISE = 'subpremise'
+
+
+# Place types supported as param in place search query.
+# Documents: https://developers.google.com/places/web-service/supported_types#table1
+PLACE_SERVICE_SEARCH_PLACE_TYPES = [
+	PlaceServicePlaceType.ACCOUNTING,
+	PlaceServicePlaceType.AIRPORT,
+	PlaceServicePlaceType.AMUSEMENT_PARK,
+	PlaceServicePlaceType.AQUARIUM,
+	PlaceServicePlaceType.ART_GALLERY,
+	PlaceServicePlaceType.ATM,
+	PlaceServicePlaceType.BAKERY,
+	PlaceServicePlaceType.BANK,
+	PlaceServicePlaceType.BAR,
+	PlaceServicePlaceType.BEAUTY_SALON,
+	PlaceServicePlaceType.BICYCLE_STORE,
+	PlaceServicePlaceType.BOOK_STORE,
+	PlaceServicePlaceType.BOWLING_ALLEY,
+	PlaceServicePlaceType.BUS_STATION,
+	PlaceServicePlaceType.CAFE,
+	PlaceServicePlaceType.CAMPGROUND,
+	PlaceServicePlaceType.CAR_DEALER,
+	PlaceServicePlaceType.CAR_RENTAL,
+	PlaceServicePlaceType.CAR_REPAIR,
+	PlaceServicePlaceType.CAR_WASH,
+	PlaceServicePlaceType.CASINO,
+	PlaceServicePlaceType.CEMETERY,
+	PlaceServicePlaceType.CHURCH,
+	PlaceServicePlaceType.CITY_HALL,
+	PlaceServicePlaceType.CLOTHING_STORE,
+	PlaceServicePlaceType.CONVENIENCE_STORE,
+	PlaceServicePlaceType.COURTHOUSE,
+	PlaceServicePlaceType.DENTIST,
+	PlaceServicePlaceType.DEPARTMENT_STORE,
+	PlaceServicePlaceType.DOCTOR,
+	PlaceServicePlaceType.ELECTRICIAN,
+	PlaceServicePlaceType.ELECTRONICS_STORE,
+	PlaceServicePlaceType.EMBASSY,
+	PlaceServicePlaceType.FIRE_STATION,
+	PlaceServicePlaceType.FLORIST,
+	PlaceServicePlaceType.FUNERAL_HOME,
+	PlaceServicePlaceType.FURNITURE_STORE,
+	PlaceServicePlaceType.GAS_STATION,
+	PlaceServicePlaceType.GYM,
+	PlaceServicePlaceType.HAIR_CARE,
+	PlaceServicePlaceType.HARDWARE_STORE,
+	PlaceServicePlaceType.HINDU_TEMPLE,
+	PlaceServicePlaceType.HOME_GOODS_STORE,
+	PlaceServicePlaceType.HOSPITAL,
+	PlaceServicePlaceType.INSURANCE_AGENCY,
+	PlaceServicePlaceType.JEWELRY_STORE,
+	PlaceServicePlaceType.LAUNDRY,
+	PlaceServicePlaceType.LAWYER,
+	PlaceServicePlaceType.LIBRARY,
+	PlaceServicePlaceType.LIQUOR_STORE,
+	PlaceServicePlaceType.LOCAL_GOVERNMENT_OFFICE,
+	PlaceServicePlaceType.LOCKSMITH,
+	PlaceServicePlaceType.LODGING,
+	PlaceServicePlaceType.MEAL_DELIVERY,
+	PlaceServicePlaceType.MEAL_TAKEAWAY,
+	PlaceServicePlaceType.MOSQUE,
+	PlaceServicePlaceType.MOVIE_RENTAL,
+	PlaceServicePlaceType.MOVIE_THEATER,
+	PlaceServicePlaceType.MOVING_COMPANY,
+	PlaceServicePlaceType.MUSEUM,
+	PlaceServicePlaceType.NIGHT_CLUB,
+	PlaceServicePlaceType.PAINTER,
+	PlaceServicePlaceType.PARK,
+	PlaceServicePlaceType.PARKING,
+	PlaceServicePlaceType.PET_STORE,
+	PlaceServicePlaceType.PHARMACY,
+	PlaceServicePlaceType.PHYSIOTHERAPIST,
+	PlaceServicePlaceType.PLUMBER,
+	PlaceServicePlaceType.POLICE,
+	PlaceServicePlaceType.POST_OFFICE,
+	PlaceServicePlaceType.REAL_ESTATE_AGENCY,
+	PlaceServicePlaceType.RESTAURANT,
+	PlaceServicePlaceType.ROOFING_CONTRACTOR,
+	PlaceServicePlaceType.RV_PARK,
+	PlaceServicePlaceType.SCHOOL,
+	PlaceServicePlaceType.SHOE_STORE,
+	PlaceServicePlaceType.SHOPPING_MALL,
+	PlaceServicePlaceType.SPA,
+	PlaceServicePlaceType.STADIUM,
+	PlaceServicePlaceType.STORAGE,
+	PlaceServicePlaceType.STORE,
+	PlaceServicePlaceType.SUBWAY_STATION,
+	PlaceServicePlaceType.SYNAGOGUE,
+	PlaceServicePlaceType.TAXI_STAND,
+	PlaceServicePlaceType.TRAIN_STATION,
+	PlaceServicePlaceType.TRANSIT_STATION,
+	PlaceServicePlaceType.TRAVEL_AGENCY,
+	PlaceServicePlaceType.UNIVERSITY,
+	PlaceServicePlaceType.VETERINARY_CARE,
+	PlaceServicePlaceType.ZOO,
+]
+
+
+# Place types could be retured in place search response.
+# Documents: https://developers.google.com/places/web-service/supported_types#table2
+PLACE_SERVICE_RESPONSE_PLACE_TYPES = [placeType 
+	for placeType in PlaceServicePlaceType.__dict__.keys() if not placeType.startswith('__')]
+
+
+# Documents: Search keyword `radius` in https://developers.google.com/places/web-service/search
+MAX_PLACE_SERACH_RADIUS_IN_METERS = 50000
--- alivee/place_search_engine/utils/place_search.py
+++ alivee/place_search_engine/utils/place_search.py
@@ -3,7 +3,8 @@ import datetime
 import json
 import time
 from place_search_engine.constants.google_place_api import PlaceSearchResponseStatus
-from place_search_engine.constants.google_place_api import PlaceSearchPlaceType
+from place_search_engine.constants.google_place_api import PLACE_SERVICE_SEARCH_PLACE_TYPES
+from place_search_engine.constants.google_place_api import MAX_PLACE_SERACH_RADIUS_IN_METERS
 
 MAX_NEXT_PAGE_TOKEN_RETRY_COUNT = 3
 
@@ -11,37 +12,63 @@ MAX_NEXT_PAGE_TOKEN_RETRY_COUNT = 3
 class PlaceSearch(object):
 	'''
 	Wrapped google place search service for our usage.
-	Documents: https://developers.google.com/places/web-service/search
+	Documents: https://developers.google.com/places/web-service/intro
+
+	Usage example: Search all banks in 1000 meters radius area of location (37.7845782, -122.3910999)
+
+	placeSearchInstance = PlaceSearch(
+		location=(37.7845782, -122.3910999), 
+		key=key,
+	)
+
+	placeSearchInstance.nearbySearch(
+		place_type=PlaceServicePlaceType.BANK, 
+		radius=1000,
+	)
+	OR:
+	placeSearchInstance.radarSearch(
+		place_type=PlaceServicePlaceType.BANK, 
+		radius=1000,
+	)
 	'''
-	def __init__(self, location, key, isVerboseMode=False):
+	def __init__(self, location, key):
 		'''
 		Init method of place search class.
 
-		param location: A tuple (lat, lng) indicates the location we want to search as the center of the area.
-		param key: The Google API key.
-		param isVerboseMode: if flag is True, then helper text will be printed for debugging.
-		return: None
+		param location: A tuple (lat, lng) represents the location we want to set as the center of the search area.
+		param key: The Google API key, generated in google dev console, ask project dev for help.
 		'''
 		self.location = location
-		self.isVerboseMode = isVerboseMode
-		# Need to handle exc in caller's method such as `ValueError: Invalid API key provided.`
+		# NOTE: Need to handle exception in caller's method such as `ValueError: Invalid API key provided.`
 		self.client = googlemaps.Client(key)
 
-	def _getPlaceFilterKwargs(self, place_type=None, radius=None, keyword=None, name=None, rank_by=None, page_token=None):
+	def _getPlaceFilterKwargs(self, place_type, radius, keyword, name, rank_by, page_token):
 		'''
-		Private helper method for creating the filter kwargs.
+		Private helper method to pre-check and create the filter kwargs in place search query, follow format of
+		https://github.com/googlemaps/google-maps-services-python/blob/master/googlemaps/places.py#L199
+
+		return: A map includes all params in place search query.
+
+		NOTE: Pre-check is needed to avoid obviously invalid API call, thus saving query count.
 		'''
 		filterKwargs = dict(location=self.location)
 
 		if place_type:
+			if not place_type in PLACE_SERVICE_SEARCH_PLACE_TYPES:
+				raise ValueError('Invalid place type, pre-check failed!'.format(place_type))
 			filterKwargs['type'] = place_type
 		if radius:
+			if radius > MAX_PLACE_SERACH_RADIUS_IN_METERS:
+				raise ValueError('Radius: {} exceeds maximum allowed value: {}, pre-check failed!'.format(
+					radius, MAX_PLACE_SERACH_RADIUS_IN_METERS))
 			filterKwargs['radius'] = radius
 		if keyword:
 			filterKwargs['keyword'] = keyword
 		if name:
 			filterKwargs['name'] = name
 		if rank_by:
+			if rank_by and radius:
+				raise ValueError('Cannot set both rank_by and radius, pre-check failed!')
 			filterKwargs['rank_by'] = rank_by
 		if page_token:
 			filterKwargs['page_token'] = page_token
@@ -50,7 +77,14 @@ class PlaceSearch(object):
 
 	def _checkReponseStatus(self, response):
 		'''
-		Check whether the status of response is valid.
+		Private helper method to check if the status of google place search response is valid.
+
+		param response: Response of google place search query, notice `googlemaps` package already parses the 
+		JSON format response to dict - list format for us.
+		return: A none-boolean value represents whether the response is valid, `None` value means response is valid
+		but there is no real content in it.
+
+		NOTE: If response is not valid, exception will be raised instead of returning `False` value.
 		'''
 		if not response or not 'status' in response:
 			raise Exception('Invalid response!')
@@ -62,94 +96,138 @@ class PlaceSearch(object):
 		elif status == PlaceSearchResponseStatus.OK:
 			return True
 
-		raise Exception('Error in response with status code {}'.format(status))
+		raise Exception('Invalid response with status code {}'.format(status))
 
 	def _parseNearbySearchResponse(self, response):
 		'''
-		Private helper method that will parse response of nearby search.
+		Private helper method to parse response of nearby search.
+
+		param response: Response of google place nearby search query.
+		return: A list of all parsed results for place search query.
+
+		NOTE: Retry for next page fetch query happens here while retry for base place search query 
+		should be handled in caller's method.
 		'''
+		allParsedResults = []
+
 		try:
-			isValidStatus = self._checkReponseStatus(response)
+			hasResult = self._checkReponseStatus(response)
 		except:
 			raise
 		else:
-			if not isValidStatus:
-				if self.isVerboseMode:
-					print 'Empty result!'
-				return
+			if hasResult:
+				resultMaps = response.get('results', [])
+				# Indicate whether there is next page of results since maximum results number show in 1 page is 20.
+				next_page_token = response.get('next_page_token')
 
-			resultMaps = response.get('results', [])
-			# Indicate whether there is next page of results
-			next_page_token = response.get('next_page_token')
-
-			if resultMaps:
-				for index, resultMap in enumerate(resultMaps, 1):
-					placeId = resultMap.get('place_id')
-					types = resultMap.get('types', [])
-					icon = resultMap.get('icon')
-					name = resultMap.get('name')
-					location = resultMap.get('geometry', {}).get('location', {})
-					if self.isVerboseMode:
-						print '--' * 20
-						print 'index: {}, name: {}, types: {}, location: {}'.format(index, name, types, location)
+				for resultMap in resultMaps:
+					allParsedResults.append(dict(
+						placeId=resultMap.get('place_id'),
+						types=resultMap.get('types', []),
+						icon=resultMap.get('icon'),
+						name=resultMap.get('name'),
+						location=resultMap.get('geometry', {}).get('location', {}),
+					))
 
 				if next_page_token:
 					retry_count = 0
-					isValidStatus = False
+					hasNextPageResult = False
 
-					while not isValidStatus and retry_count <= MAX_NEXT_PAGE_TOKEN_RETRY_COUNT:
-						# Sometimes there is delay when next_page_token is issued, sleep 1s and retry 3 times.
+					while not hasNextPageResult and retry_count <= MAX_NEXT_PAGE_TOKEN_RETRY_COUNT:
+						# Sometimes there is delay when `next_page_token` is issued, sleep 2 seconds and retry 3 times.
 						time.sleep(2)
 
-						if retry_count and self.isVerboseMode:
-							print 'Retry {} times...'.format(retry_count)
+						if retry_count:
+							print 'Retrying {} times for fetching next page results...'.format(retry_count)
 						try: 
-							self.nearbySearch(page_token=next_page_token)
-						except Exception as e:
+							nextPageParsedResults = self.nearbySearch(page_token=next_page_token, isVerboseMode=False)
+						except:
 							retry_count += 1
-							if self.isVerboseMode:
-								print 'Exception {} when fetching next page by using token.'.format(e)
-								if retry_count > MAX_NEXT_PAGE_TOKEN_RETRY_COUNT:
-									print 'Already reached maximum retry times: {}, exit...'.format(MAX_NEXT_PAGE_TOKEN_RETRY_COUNT)
+							if retry_count > MAX_NEXT_PAGE_TOKEN_RETRY_COUNT:
+								print 'Already reached maximum next page retry times: {}.'.format(
+									MAX_NEXT_PAGE_TOKEN_RETRY_COUNT)
 						else:
-							isValidStatus = True
+							hasNextPageResult = True
+							allParsedResults.extend(nextPageParsedResults)
+
+		return allParsedResults
 
 	def _parseRadarSearchResponse(self, response):
 		'''
 		Private helper method that will parse response of radar search.
+
+		param response: Response of google place radar search query.
+		return: A list of all parsed results for place search query.
 		'''
+		allParsedResults = []
+
 		try:
-			isValidStatus = self._checkReponseStatus(response)
+			hasResult = self._checkReponseStatus(response)
 		except:
 			raise
 		else:
-			if not isValidStatus:
-				if self.isVerboseMode:
-					print 'Empty result!'
-				return
+			if hasResult:
+				resultMaps = response.get('results', [])
 
-			resultMaps = response.get('results', [])
+				for resultMap in resultMaps:
+					allParsedResults.append(dict(
+						placeId=resultMap.get('place_id'),
+						location=resultMap.get('geometry', {}).get('location', {}),
+					))
 
-			if resultMaps:
-				for index, resultMap in enumerate(resultMaps, 1):
-					placeId = resultMap.get('place_id')
-					location = resultMap.get('geometry', {}).get('location', {})
-					if self.isVerboseMode:
-						print '--' * 20
-						print 'index: {}, location: {}'.format(index, location)
+		return allParsedResults
 
-	def nearbySearch(self, place_type=None, radius=None, keyword=None, name=None, rank_by=None, page_token=None):
+	def nearbySearch(self, place_type=None, radius=None, keyword=None, name=None, rank_by=None, page_token=None, 
+		isVerboseMode=True):
 		'''
-		Google nearby search, 20 results max for 1 page and 60 results max for 1 query.
+		Wrapped google place nearby search, 20 results max for 1 page and 60 results max for 1 query.
+		Documents: https://developers.google.com/places/web-service/search
+
+		param isVerboseMode: A boolean flag indicates if helper text will be printed for debugging.
+		return: A list of all parsed results for place search query.
+
+		NOTE: Not all params included for query search such as `minprice` and `maxprice`, if needed, more params
+		can be added later.
 		'''
 		filterKwargs = self._getPlaceFilterKwargs(place_type, radius, keyword, name, rank_by, page_token)
 		response = self.client.places_nearby(**filterKwargs)
-		self._parseNearbySearchResponse(response)
+		allParsedResults = self._parseNearbySearchResponse(response)
 
-	def radarSearch(self, place_type=None, radius=None, keyword=None, name=None, rank_by=None):
+		if isVerboseMode:
+			if not allParsedResults:
+				print 'No results found in nearby response, do nothing!'
+			else:
+				for index, parsedResult in enumerate(allParsedResults, 1):
+					print '--' * 20
+					# For unicode output, see https://pythonhosted.org/kitchen/unicode-frustrations.html
+					print 'index: {}, name: {}, location: {}'.format(
+						index, parsedResult.get('name').encode('utf8', 'replace'), parsedResult.get('location'))
+					print 'placeId: {}, icon: {}'.format(parsedResult.get('placeId'), parsedResult.get('icon'))
+
+		return allParsedResults
+
+	def radarSearch(self, place_type=None, radius=None, keyword=None, name=None, rank_by=None, isVerboseMode=True):
 		'''
-		Google radar search, 200 results max for 1 query, less details included.
+		Wrapped google place radar search, 200 results max for 1 query, less details included.
+		Documents: https://developers.google.com/places/web-service/search#RadarSearchRequests
+
+		param isVerboseMode: A boolean flag indicates if helper text will be printed for debugging.
+		return: A list of all parsed results for place search query.
+
+		NOTE: Not all params included for query search such as `minprice` and `maxprice`, if needed, more params
+		can be added later.
 		'''
-		filterKwargs = self._getPlaceFilterKwargs(place_type, radius, keyword, name, rank_by)
+		filterKwargs = self._getPlaceFilterKwargs(place_type, radius, keyword, name, rank_by, page_token=None)
 		response = self.client.places_radar(**filterKwargs)
-		self._parseRadarSearchResponse(response)
+		allParsedResults = self._parseRadarSearchResponse(response)
+
+		if isVerboseMode:
+			if not allParsedResults:
+				print 'No results found in radar response, do nothing!'
+			else:
+				for index, parsedResult in enumerate(allParsedResults, 1):
+					print '--' * 20
+					print 'index: {}, placeId: {}, location: {}'.format(
+						index, parsedResult.get('placeId'), parsedResult.get('location'))
+
+		return allParsedResults
